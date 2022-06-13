499a153a691aa23351100e601cb01c74da3000d3
Ali <aliabdullah@jumpstart.security>
2022-06-14 00:24:39 +0500

    Created Login and SignUp Screens.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,146 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React, { useState } from 'react';
+import { View, Text, StatusBar, StyleSheet, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import AuroraButton from '../components/AuroraButton';
+import Logo from '../components/logo';
+import Feather from 'react-native-vector-icons/Feather';
+// import { Overlay } from 'react-native-elements';
+import PopUp from '../components/popup';
+import Loader from '../components/Loader';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function SignUp({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [overlay, setOverlay] = useState(false);
+  const [OverlayText, setOverlayText] = useState('Registration is currently not complete');
+  const [popUpErr, setpopUpErr] = useState(false);
+  const [Registered, setRegistered] = useState(false);
+  // const [Fullname,onChangeFullname] = useState('');
+  const [Email, onChangeEmail] = useState('');
+  // const [Mobile,onChangeMobile] = useState('');
+  const [Password, onChangePassword] = useState('');
+  const [ConfirmPassword, onChangeConfirmPassword] = useState('');
+  const [loader, setLoader] = useState(false);
+
+  const createAccount = () => {
+    /// Api Call for signup ///
+  }
+
+
+  const SignUpSuccess = () => {
+    setRegistered(false);
+    setLoader(false);
+    setOverlay(false);
+    // onChangeFullname('');
+    onChangeConfirmPassword('');
+    onChangeEmail('');
+    // onChangeMobile('');
+    onChangePassword('');
+    // navigation.navigate('SignIn');
+  }
+
+
+  const validate = () => {
+    //validate the full name
+    // var fullname_has_spacing = Fullname.includes(" ");
+    // var index_of_spacing = Fullname.indexOf(" ");
+    // var second_name = Fullname.substring(index_of_spacing+1);
+    // var first_name = Fullname.substring(0,index_of_spacing);
+    //
+    if (Email || Password || ConfirmPassword == '') {
+      showErr(true, true, "Unable to sign up \n please fill in all fields");
+    }
+    // else if(isNaN(Mobile) || Mobile.length < 10 || Mobile[0] != '0' || Mobile.length > 10){
+    //     showErr(true,true,"Unable to sign up \n please enter a valid 10 digit mobile number");
+
+    if (Password != ConfirmPassword) {
+      showErr(true, true, "Unable to sign up \n your passwords do not match");
+    }
+    // else if(fullname_has_spacing == false){
+    //     showErr(true,true,"Unable to sign up \n please enter your fullname");
+
+    // else if(first_name.length < 2){
+    //     showErr(true,true,"Unable to sign up \n a valid first name must have 2 characters or more");
+    // }
+    // else if(second_name.length < 2){
+    //     showErr(true,true,"Unable to sign up \n a valid last name must have 2 characters or more");
+    // }
+    if (Password.length < 6) {
+      showErr(true, true, "Unable to sign up \n a password must be at least 6 characters long");
+    }
+    else {
+      createAccount();
+    }
+  }
+
+  const showErr = (show_overlay: boolean, show_popup_err: boolean, overlay_text: string) => {
+    setOverlay(show_overlay);
+    setpopUpErr(show_popup_err);
+    setOverlayText(overlay_text);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+    <>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <View style={styles.body}>
+        <View style={styles.topSection}>
+          <Logo />
+        </View>
+
+        <ScrollView style={styles.bottomSection} contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
+          <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <View style={{ width: '95%', height: 50 }}>
+              {/* <TouchableOpacity onPress={()=>navigation.navigate('Main')}>
+                                <Feather size={50} color={'#fff'} name="chevron-left"/>
+                            </TouchableOpacity> */}
+            </View>
+            <View style={{ width: '80%', marginBottom: 20 }}>
+              <Text style={{ color: '#fff', fontSize: 40, marginTop: 10, marginBottom: 30 }}>Create {'\n'}Account</Text>
+              {/* <TextInput value={Fullname} onChangeText={text => onChangeFullname(text)} style={{height:52,width:'100%',color:'white',borderRadius:10,borderWidth:1,borderColor:'white',paddingLeft:10}} placeholderTextColor="#fff" placeholder="Full name" />   */}
+              <TextInput value={Email} onChangeText={text => onChangeEmail(text)} style={{ height: 52, width: '100%', color: 'white', borderRadius: 10, borderWidth: 1, borderColor: 'white', paddingLeft: 10, marginTop: 10 }} placeholderTextColor="#fff" placeholder="Email" />
+              {/* <TextInput value={Mobile} onChangeText={text => onChangeMobile(text)} style={{height:52,width:'100%',color:'white',borderRadius:10,borderWidth:1,borderColor:'white',paddingLeft:10,marginTop:10}} placeholderTextColor="#fff" placeholder="Mobile" /> */}
+              <TextInput value={Password} onChangeText={text => onChangePassword(text)} secureTextEntry={true} style={{ height: 52, width: '100%', color: 'white', borderRadius: 10, borderWidth: 1, borderColor: 'white', paddingLeft: 10, marginTop: 10 }} placeholderTextColor="#fff" placeholder="Password" />
+              <TextInput value={ConfirmPassword} onChangeText={text => onChangeConfirmPassword(text)} secureTextEntry={true} style={{ height: 52, width: '100%', color: 'white', borderRadius: 10, marginTop: 10, borderWidth: 1, borderColor: 'white', paddingLeft: 10 }} placeholderTextColor="#fff" placeholder="Confirm Password" />
+            </View>
+            <AuroraButton buttonFunction={() => validate()} bgcolor="white" text="Sign up" color={"black"} outline={false} />
+          </View>
+        </ScrollView>
+      </View>
+      {/* <Overlay isVisible={overlay}>
+        <PopUp errorBtn={() => setOverlay(false)} text={OverlayText} error={popUpErr} />
+      </Overlay>
+
+      <Overlay isVisible={Registered}>
+        <PopUp successBtn={() => SignUpSuccess()} text={"Welcome to Aurora, please check your inbox to verify your account."} />
+      </Overlay>
+
+      <Overlay isVisible={loader}>
+        <Loader text={'Creating your account, please wait..'} />
+      </Overlay> */}
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  topSection: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: '20%',
+
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  body: {
+    flex: 1,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  bottomSection: {
+    width: '100%',
+    height: '80%',
+    backgroundColor: '#000',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    // justifyContent:'center',
+    // alignItems:'center'
+  }
+
+})
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,31 +1,102 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StatusBar, StyleSheet, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import AuroraButton from '../components/AuroraButton';
+import Logo from '../components/logo';
+import Feather from 'react-native-vector-icons/Feather';
+// import { Overlay } from 'react-native-elements';
+import PopUp from '../components/popup';
+import Loader from '../components/loader';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+export default function Login() {
+  const [overlay, setOverlay] = useState(false);
+  const [OverlayText, setOverlayText] = useState("");
+  const [popUpErr, setpopUpErr] = useState(false);
+  const [email, onChangeEmail] = useState('');
+  const [password, onChangePassword] = useState('');
+  const [loader, setLoader] = useState(false);
 
-export default function TabTwoScreen() {
+  const signIn = () => {
+    /// Api Call For SignIn
+
+    /// on successfull login, navigation.navigate('Modal') ///
+  }
+
+  const validate = () => {
+    if (email == '' && password == '') {
+      showErr(true, true, "Unable to sign in \n please fill in all fields");
+    } else if (email == '') {
+      showErr(true, true, "Unable to sign in \n please enter your email");
+    } else if (password == '') {
+      showErr(true, true, "Unable to sign in \n please enter your password");
+    } else {
+      setOverlay(false);
+      setLoader(true);
+      signIn();
+    }
+  }
+
+
+  const showErr = (show_overlay: boolean, show_popup: boolean, overlay_text: string) => {
+    setOverlay(show_overlay);
+    setpopUpErr(show_popup);
+    setOverlayText(overlay_text);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
-}
+    <>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <View style={styles.body}>
+        <View style={styles.topSection}>
+          <Logo />
+        </View>
 
+        <ScrollView style={styles.bottomSection} contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
+          <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <View style={{ width: '95%', height: 50 }}>
+              {/* <TouchableOpacity onPress={()=>navigation.navigate('Main')}>
+                        <Feather size={50} color={'#fff'} name="chevron-left"/>
+                    </TouchableOpacity> */}
+            </View>
+            <View style={{ width: '80%', marginBottom: 20 }}>
+              <Text style={{ color: '#fff', fontSize: 40, marginTop: 10, marginBottom: 30 }}>Welcome {'\n'}Back</Text>
+              <TextInput value={email} onChangeText={text => onChangeEmail(text)} style={{ height: 52, width: '100%', color: 'white', borderRadius: 10, borderWidth: 1, borderColor: 'white', paddingLeft: 10 }} placeholderTextColor="#fff" placeholder="Email" />
+              <TextInput value={password} onChangeText={text => onChangePassword(text)} secureTextEntry={true} style={{ height: 52, width: '100%', color: 'white', borderRadius: 10, marginTop: 10, borderWidth: 1, borderColor: 'white', paddingLeft: 10 }} placeholderTextColor="#fff" placeholder="Password" />
+            </View>
+            <AuroraButton buttonFunction={() => validate()} bgcolor="white" text="Sign in" color={"black"} outline={false} />
+            {/* <TouchableOpacity onPress={()=>navigation.navigate('ForgotPassword')}>
+                      <Text style={{color:'white',marginTop:20,color:'#242424',textDecorationLine:'underline'}}>Forgot password</Text>
+                </TouchableOpacity> */}
+          </View>
+        </ScrollView>
+      </View>
+      {/* <Overlay isVisible={overlay}>
+            <PopUp errorBtn={()=>setOverlay(false)} text={OverlayText} error={popUpErr} />
+        </Overlay>
+        <Overlay isVisible={loader}>
+            <Loader text={'Signing you in, please wait..'}/>
+        </Overlay> */}
+    </>
+  )
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  topSection: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: '30%',
+
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  body: {
+    flex: 1,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  bottomSection: {
+    width: '100%',
+    height: '70%',
+    backgroundColor: '#000',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  }
+
+})
